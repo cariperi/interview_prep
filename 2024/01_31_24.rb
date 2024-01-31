cbaa5572ec42a889f809751736192ab6e1f05993
Caroline Peri <caroline.anne.peri@gmail.com>
2024-01-31 18:53:32 -0500

    template: Supermarket Queue 1-31-24

---
+++ 2024/01_31_24.rb
@@ -0,0 +1,29 @@
+# CodeWars Kata: The Supermarket Queue
+# https://www.codewars.com/kata/57b06f90e298a7b53d000a86
+
+def queue_time(customers, n)
+  #your code here
+end
+
+# INPUT DATA TYPE
+
+
+# OUTPUT DATA TYPE
+
+
+# ASSUMPTIONS
+
+
+# QUESTIONS
+
+
+#################################
+# TEST CASES - UNCOMMENT TO RUN #
+#################################
+
+# p queue_time([], 1) #0
+# p queue_time([5], 1) #5
+# p queue_time([5,3,4], 1) #12
+# p queue_time([10,2,3,3], 2) #10
+# p queue_time([2,3,10], 2) #12
+# p queue_time([1,2,3,4,5], 100) #5
