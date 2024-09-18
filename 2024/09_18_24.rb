a890c1979d06580ba60abe29742d9b1ffaae52da
Caroline Peri <caroline.anne.peri@gmail.com>
2024-09-18 18:48:48 -0400

    initial empty problem: 09-18-24

---
+++ 2024/09_18_24.rb
@@ -0,0 +1,39 @@
+# Smart Traffic Lights (OOP)
+# https://www.codewars.com/kata/58587905ed1b4dad6e0000c6
+
+# frozen_string_literal: true
+
+class SmartTrafficLight
+  def initialize(st1, st2)
+
+  end
+
+  def turngreen()
+
+  end
+end
+
+#######################
+# PRE-CODING PLANNING #
+#######################
+
+# INPUT DATA TYPE
+
+
+# OUTPUT DATA TYPE
+
+
+# ASSUMPTIONS
+
+
+# QUESTIONS
+
+
+#################################
+# TEST CASES - UNCOMMENT TO RUN #
+#################################
+
+t = SmartTrafficLight.new([42, '27th ave'], [72, '3rd st'])
+t.turngreen() #'3rd st'
+t.turngreen() #'27th ave'
+t.turngreen() #nil
