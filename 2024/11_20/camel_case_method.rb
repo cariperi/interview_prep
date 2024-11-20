c82c7721f398728b70ca4b4a9412f5befa1516cd
Caroline Peri <caroline.anne.peri@gmail.com>
2024-11-20 18:52:49 -0500

    working outline for 11.20.24 exercise - CamelCase Method

---
+++ 2024/11_20/camel_case_method.rb
@@ -0,0 +1,24 @@
+# https://www.codewars.com/kata/587731fda577b3d1b0001196
+# Write a method (or function, depending on the language) that converts a string to camelCase
+# That is, all words must have their first letter capitalized and spaces must be removed
+
+class String
+  def camelcase
+  end
+end
+
+# INPUT
+
+
+# OUTPUT
+
+
+# ASSUMPTIONS AND QUESTIONS
+
+
+# TESTS - UNCOMMENT TO RUN
+
+# pp 'test case'.camelcase == 'TestCase'
+# pp 'camel case method'.camelcase == 'CamelCaseMethod'
+# pp 'say hello '.camelcase == 'SayHello'
+# pp ' camel case word'.camelcase == 'CamelCaseWord'
