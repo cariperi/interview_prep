fbe5e72704567c27f55e85fcef0b3f08a0ec3d6d
Caroline Peri <caroline.anne.peri@gmail.com>
2024-12-18 18:47:10 -0500

    exercise framework for 12.18.24 session

---
+++ 2024/12_18.rb
@@ -0,0 +1,24 @@
+# Steps in Primes
+# Codewars: https://www.codewars.com/kata/5613d06cee1e7da6d5000055
+# Liveshare: https://prod.liveshare.vsengsaas.visualstudio.com/join?412C89ECE6A9444836B4E879814B55C5BE8D
+
+def step(step_size, search_start, search_end)
+  # your code
+end
+
+# INPUT
+
+
+# OUTPUT
+
+
+# ASSUMPTIONS AND QUESTIONS
+
+
+# TESTS (uncomment to run)
+
+# pp step(2,100,110) #[101, 103]
+# pp step(4,100,110) #[103, 107]
+# pp step(6,100,110) #[101, 107]
+# pp step(8,300,400) #[359, 367]
+# pp step(10,300,400)  #[307, 317]
