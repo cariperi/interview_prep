388626ecb0b82104eb3c8f726300e1348ef5f059
Caroline Peri <caroline.anne.peri@gmail.com>
2025-03-12 18:55:47 -0400

    setup for 03/12 interview prep session

---
+++ 2025/03_12.rb
@@ -0,0 +1,18 @@
+# Write Number in Expanded Form
+# Codewars: https://www.codewars.com/kata/write-number-in-expanded-form
+
+def expanded_form(num)
+  # Your code here
+end
+
+# ASSUMPTIONS AND QUESTIONS
+
+# INPUT
+
+# OUTPUT
+
+# EXAMPLE AND TESTS
+
+pp expanded_form(12) #"10 + 2"
+pp expanded_form(45) #"40 + 5"
+pp expanded_form(70304) #"70000 + 300 + 4"
