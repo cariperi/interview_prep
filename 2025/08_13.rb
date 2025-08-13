ad7bb05ef3ed89d46d5333ec9886248dc70a25cd
Caroline Peri <caroline.anne.peri@gmail.com>
2025-08-13 18:49:39 -0400

    blank template - 08.13

---
+++ 2025/08_13.rb
@@ -0,0 +1,24 @@
+# String Scramble
+# https://www.codewars.com/kata/5822d89270ca28c85c0000f3
+
+def scramble(str, arr)
+  # add code here
+end
+
+
+# INPUT
+
+
+# OUTPUT
+
+
+# QUESTIONS AND ASSUMPTIONS
+
+
+# PSEUDOCODE
+
+
+# EXAMPLES AND TESTS - Uncomment to Run
+  # pp scramble('abcd', [0,3,1,2]) == 'acdb'
+  # pp scramble('sc301s', [4,0,3,1,5,2]) == "c0s3s1"
+  # pp scramble('bskl5', [2,1,4,3,0]) == "5sblk"
