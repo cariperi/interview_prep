101cf5e6edb5e7f3082500a6dd3b8abe02bdc164
Caroline Peri <caroline.anne.peri@gmail.com>
2025-08-29 16:01:17 -0400

    working solution - 8.13.25

--- 2025/08_13.rb
+++ 2025/08_13.rb
@@ -3,22 +3,36 @@
 
 def scramble(str, arr)
   # add code here
+  result = Array.new(arr.length)
+
+  str.chars.each_with_index do |char, index|
+    result[arr[index]] = char
+  end
+
+  result.join
 end
 
 
 # INPUT
-
+# string
+# array of integers (indices)
 
 # OUTPUT
-
+#string
 
 # QUESTIONS AND ASSUMPTIONS
-
+# Is it ok to use auxillary space? (Assume yes)
+# All indices are valid
+# All string chars are valid chars
 
 # PSEUDOCODE
-
+# instantiate an empty array of length == string (or array input)
+# iterate over the string values (string.chars.each_with_index) --> better approach because IF we wanted to extend to unicode chars, in Ruby these chars are not a fixed length
+# each loop, have the char and the index i, new arr index i' from array with i
+# then we'll insert the char into the new array at i'
+# last step is join the new array to make a string and return
 
 # EXAMPLES AND TESTS - Uncomment to Run
-  # pp scramble('abcd', [0,3,1,2]) == 'acdb'
-  # pp scramble('sc301s', [4,0,3,1,5,2]) == "c0s3s1"
-  # pp scramble('bskl5', [2,1,4,3,0]) == "5sblk"
+  pp scramble('abcd', [0,3,1,2]) == 'acdb'
+  pp scramble('sc301s', [4,0,3,1,5,2]) == "c0s3s1"
+  pp scramble('bskl5', [2,1,4,3,0]) == "5sblk"
